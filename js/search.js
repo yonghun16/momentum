dd523a500f4e3bda987c47c4b67cdd1631d101a2
yonghun16 <yonghun16@naver.com>
2023-10-07 22:07:45 +0900

    update

--- js/search.js
+++ js/search.js
@@ -1,10 +1,52 @@
-const searchForm = document.querySelector("#search-form");
-const searchInput = document.querySelector("#search-form input");
+const searchForm = document.querySelector(".searchForm");
+const searchFormGlass = document.querySelector(".searchForm__glass");
+const searchInput = document.querySelector(".searchForm input");
+const searchEngin = document.querySelector(".searchForm__engin");
 
+// searchForm에 마우스가 들어 왔을 때 이벤트
+function hoverSearch() {
+  if(!searchForm.classList.contains("searchForm-focus")) {
+    searchForm.classList.add("searchForm-hover"); 
+    searchFormGlass.classList.add("searchForm__glass-hover");
+    searchEngin.classList.add("searchForm__engin-hover")
+  }
+}
 
+// searchForm에서 마우스가 벗어 났을 때 이벤트
+function leaveSearch() {
+  searchForm.classList.remove("searchForm-hover"); 
+  searchFormGlass.classList.remove("searchForm__glass-hover");
+  searchEngin.classList.remove("searchForm__engin-hover")
+}
+
+// inputbox focus 선택 이벤트
+function focusSearch() {
+  searchForm.classList.remove("searchForm-hover"); 
+  searchForm.classList.add("searchForm-focus");
+  searchFormGlass.classList.remove("searchForm__glass-hover");
+  searchFormGlass.classList.add("searchForm__glass-focus");
+  searchEngin.classList.remove("searchForm__engin-hover");
+  searchEngin.classList.add("searchForm__engin-focus");
+}
+
+// inputbox focus가 바뀌었을 때 이벤트
+function focusChangeSearch() {
+  searchForm.classList.remove("searchForm-focus");
+  searchFormGlass.classList.remove("searchForm__glass-focus");
+  searchEngin.classList.remove("searchForm__engin-focus");
+}
+
+// submit 이벤트
 function onSearchSubmit() {
   const searchItem = searchInput.value;
   console.log(searchItem);
 }
+
+//이벤트 핸들링
+searchForm.addEventListener("mouseover", hoverSearch);
+searchForm.addEventListener("mouseout", leaveSearch);
+
+searchInput.addEventListener("focus", focusSearch);
+searchInput.addEventListener("blur", focusChangeSearch);
 
 searchForm.addEventListener("submit", onSearchSubmit);
